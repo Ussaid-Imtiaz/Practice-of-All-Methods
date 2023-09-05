45c2f1d1a2e873b1befd2d18f380a4e359b92853
Ussaid Imtiaz <130559788+Ussaid-Imtiaz@users.noreply.github.com>
2023-09-05 10:35:11 +0000

    more methods

--- app.ts
+++ app.ts
@@ -33,6 +33,8 @@ let arr2: number[] = [1, 54, 78, 68, 98, 45];
 // console.log(arr2)                   //      [ 1, 100, 68, 98, 45 ]  added item at index 1=100
 
 //============== Non-Mutating Methods does not change the original array ============================
+let numArr1 = [1,2,3,4,5,6,7,8]
+let numArr2 = [6,7,8,9,10,11,12]
 
 let newArr = myArr.concat(arr2 as unknown as string)   // type casting
 // console.log(newArr)   //  ['Maryam', 'Maryam','Rameen', 1,54,78,68,98,45] 
@@ -61,22 +63,20 @@ const filterArr2 = myArr.filter((word) => word.includes("e"));
 // console.log(filterArr2)          //      [ 'Rameen' ]      string with e filtered
 // console.log(myArr)               //      [ 'Ussaid', 'Maryam', 'Rameen' ]
 
-let numArr1 = [1,2,3,4,5,6,7,8]
-let numArr2 = [6,7,8,9,10,11,12]
 const commonItems = numArr1.filter(item => numArr2.indexOf(item) !== -1)
 // console.log(commonItems)             //      [ 6, 7, 8 ]
+
 const uncommonItems = numArr1.filter(item => numArr2.indexOf(item) == -1).concat(numArr2.filter(item => numArr1.indexOf(item) == -1)) ;
 // console.log(uncommonItems)      //      [1,  2,  3,  4, 5, 9, 10, 11, 12]
 
-// const numArr1 = [1, 2, 3];
-// const numArr2 = [2, 3, 4, 5];
-
-// const uncommonItems = numArr2.filter((item) => !numArr1.includes(item));
-
-// console.log(uncommonItems); // Output: [4, 5]
+const fruits = ['apple', 'banana', 'cherry', 'date'];
+let allParametersFilter = fruits.filter((item, index, array) => {
+    return item.length > 5 && index % 2 === 0;
+})
+console.log(allParametersFilter)
 
 let mapArr = numArr2.map(value => value * 2)
-// console.log(mapArr)         //      [12, 14, 16, 18, 20, 22, 24]
+// console.log(mapArr)                  //      [12, 14, 16, 18, 20, 22, 24]
 
 let everyArr = myArr.every((word, index, array) => {
     return index < 4 && word.length % 2 === 0;
@@ -90,7 +90,7 @@ let everyArr2 = numArr3.every((num, index, array) => {
     }
     return num % 2 === 0 && num > array[index - 1]
 })
-// console.log("Result:", everyArr2)       //  true
+// console.log("Result:", everyArr2)       //       true
 
 
 
