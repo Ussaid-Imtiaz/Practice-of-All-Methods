b29dbab88bba69d9467c9dbf00db1555eb4c8019
Ussaid <imtiazussaid@gmail.com>
2023-09-11 21:25:56 +0500

    new changes

--- arrays/arrayExercises.js
+++ arrays/arrayExercises.js
@@ -9,6 +9,19 @@ const peopleOver30 = people.filter((item, index, array) => item.age > 30);
 // console.log(peopleOver30)       //      [ { name: 'Bob', age: 32 } ]
 const numbers = [15, 7, 22, 45, 98, 31, 6];
 // Your task is to find the largest number here
+//                     array  method  previous     curent        type        if                          then          else                 initial           
 const largestNumber = numbers.reduce((previousValue, currentValue) => (currentValue > previousValue ? currentValue : previousValue), numbers[0]);
-console.log(largestNumber);
+// console.log(largestNumber)
+const numbers1 = [8, 17, 42, 99, 23, 12, 6];
+// Your task is to calculate the sum of even numbers here
+const sumOfEvenNumbers = numbers1.reduce((accumulator, currentValue) => (currentValue % 2 === 0 ? accumulator + currentValue : currentValue), 0);
+// console.log(sumOfEvenNumbers)
+const fruits = ["apple", "banana", "orange", "banana", "kiwi", "apple"];
+// Your task is to remove duplicates here
+const removeDuplicates = fruits.filter((value, index, array) => array.indexOf(value) === index);
+console.log(removeDuplicates);
+const nestedArray = [1, [2, 3], [4, [5, 6]], 7];
+// Your task is to flatten the nested array here
+const flatArray = nestedArray.flat(Infinity);
+console.log(flatArray);
 export {};
--- arrays/arrayMethods/push.ts
+++
@@ -1,6 +0,0 @@
-let myArr: string[] = ["Ussaid", "Maryam", "Rameen"];
-
-let pushArr = myArr.push("Ussaid");
-console.log(pushArr.toString())    //  return index no. 4 as string
-console.log(myArr)  //[ 'Maryam', 'Maryam', 'Rameen', 'Ussaid' ]
-
